2f5b097d25a91060d835b57d11b1855bf7dbe0a0
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2024-05-15 13:32:36 -0300

    ajustes de cor, fonte e informações na tela de login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -30,42 +30,51 @@ const Login: React.FC<LoginProps> = ({ onSubmit }) => {
     
     <View style={{width: '100%',
      height: '100%', 
+     paddingTop: '40%',
      backgroundColor: '#ff3300', 
-     justifyContent: 'center', 
      flex: 1, 
      alignItems: 'center'}}>
-        <Image style={{width: '40%', height: '20%'}} source={require('../assets/emi-logo.png')} />
+        <Image style={{width: 200, height: 200}} source={require('../assets/emi-logo.png')} />
+        <Text style={{color: '#ffffff', fontSize: 26, fontWeight: 'bold', padding: 10}}>Login</Text>
+        <View style={{width: '80%'}}>
+        <Text style={{paddingLeft: 10, paddingBottom: 8, color: '#ffffff', fontSize: 16, fontWeight: 'bold'}}>Email:</Text>
       <TextInput
-      style={{width: '80%',
+      style={{width: '100%',
+      backgroundColor: '#ffffff',
       marginBottom: 10,
       paddingHorizontal: 10,
       paddingVertical: 8,
       borderWidth: 1,
-      color: '#ffffff',
+      color: '#0000000',
       borderColor: '#ccc',
-      borderRadius: 5
-      }}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        />
+      borderRadius: 10
+    }}
+    placeholder="Email"
+    value={email}
+    onChangeText={setEmail}
+    />
+    <Text style={{paddingLeft: 10, paddingBottom: 8, color: '#ffffff', fontSize: 16, fontWeight: 'bold'}}>Senha: </Text>
       <TextInput
-    style={{width: '80%',
+    style={{width: '100%',
+    backgroundColor: '#ffffff',
     marginBottom: 10,
     paddingHorizontal: 10,
     paddingVertical: 8,
     borderWidth: 1,
-    color: '#ffffff',
+    color: '#0000000',
     borderColor: '#ccc',
-    borderRadius: 5
+    borderRadius: 10
     }}
         placeholder="Password"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
       />
+        </View>
+        
       {/* <Button title="Login" onPress={handleLogin} style={styles.button}/> */}
       <Button title="Login" onPress={handleLogin} />
+      <Text style={{color: '#ffffff', fontSize: 16, padding: 10, textDecorationLine: 'underline'}}>Cadastre-se</Text>
     </View>
   )
 }
